f9f196588c66efd2a3d1c6e2ce481b0d85c79481
mfebres0919 <guellefeb21@gmail.com>
2025-05-27 22:16:02 -0500

    Updated Files in Main Branch Location

--- assets/js/agriox.js
+++ assets/js/agriox.js
@@ -607,19 +607,9 @@
         },
       },
       submitHandler: function (form) {
-        // sending value with ajax request
-        $.post(
-          $(form).attr("action"),
-          $(form).serialize(),
-          function (response) {
-            $(form).parent().find(".result").append(response);
-            $(form).find('input[type="text"]').val("");
-            $(form).find('input[type="email"]').val("");
-            $(form).find("textarea").val("");
-          }
-        );
-        return false;
-      },
+  form.submit();
+  },
+
     });
   }
 
